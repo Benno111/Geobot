8b572aacd5a6336ae97a16b493064018d9b94808
Benno111 <80102205+Benno111@users.noreply.github.com>
2026-07-13 16:52:24 +0200

    Remove MenuLayer startup overlay hook

--- src/keybinds.cpp
+++ src/keybinds.cpp
@@ -1,14 +1,6 @@
 #include "includes.hpp"
-#include "ui/record_layer.hpp"
-#include "ui/game_ui.hpp"
-#include "ui/clickbot_layer.hpp"
-#include "ui/macro_editor.hpp"
-#include "ui/render_settings_layer.hpp"
-#include "hacks/layout_mode.hpp"
-#include "hacks/show_trajectory.hpp"
 
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
-#include <Geode/modify/CCTouchDispatcher.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 
 #include <array>
@@ -17,9 +9,7 @@
 namespace {
 using Clock = std::chrono::steady_clock;
 int s_developerKeyProgress = 0;
-int s_developerTapProgress = 0;
 Clock::time_point s_lastDeveloperKeyAt = {};
-Clock::time_point s_lastDeveloperTapAt = {};
 
 bool advanceDeveloperKeySequence(
   int& progress,
@@ -55,27 +45,6 @@ bool advanceDeveloperKeySequence(
   return false;
 }
 
-bool advanceDeveloperTapSequence(
-  int& progress,
-  Clock::time_point& lastTapAt
-) {
-  constexpr int kTapCount = 5;
-  constexpr auto kTimeout = std::chrono::milliseconds(1250);
-  auto now = Clock::now();
-
-  if (progress <= 0 || now - lastTapAt > kTimeout)
-    progress = 1;
-  else
-    progress++;
-
-  lastTapAt = now;
-  if (progress >= kTapCount) {
-    progress = 0;
-    return true;
-  }
-  return false;
-}
-
 void showDeveloperModeNotification(bool enabled) {
   Notification::create(
     enabled ? "Developer mode enabled" : "Developer mode disabled",
@@ -89,25 +58,6 @@ void toggleDeveloperMode() {
   showDeveloperModeNotification(enabled);
 }
 
-class DeveloperModeTapTarget : public CCNode {
-public:
-  static DeveloperModeTapTarget* create() {
-    auto* ret = new DeveloperModeTapTarget();
-    if (ret && ret->init()) {
-      ret->autorelease();
-      return ret;
-    }
-    delete ret;
-    return nullptr;
-  }
-
-  void onTap(CCObject*) {
-    if (!advanceDeveloperTapSequence(s_developerTapProgress, s_lastDeveloperTapAt))
-      return;
-
-    toggleDeveloperMode();
-  }
-};
 }
 
 class $modify(CCKeyboardDispatcher) {
@@ -127,31 +77,6 @@ class $modify(CCKeyboardDispatcher) {
   }
 };
 class $modify(MenuLayer) {
-  bool init() {
-    if (!MenuLayer::init())
-      return false;
-
-    auto winSize = CCDirector::sharedDirector()->getWinSize();
-    auto touchMenu = CCMenu::create();
-    touchMenu->setPosition({ 0.f, 0.f });
-    touchMenu->setID("developer-mode-unlock-menu"_spr);
-    auto tapTarget = DeveloperModeTapTarget::create();
-    addChild(tapTarget);
-
-    auto touchTarget = CCLayerColor::create({ 0, 0, 0, 0 }, 150, 70);
-    auto unlockButton = CCMenuItemSpriteExtra::create(
-      touchTarget,
-      tapTarget,
-      menu_selector(DeveloperModeTapTarget::onTap)
-    );
-    unlockButton->setPosition({ winSize.width / 2.f, winSize.height - 52.f });
-    unlockButton->setID("developer-mode-unlock-button"_spr);
-    touchMenu->addChild(unlockButton);
-
-    addChild(touchMenu, 999);
-    return true;
-  }
-
   void keyDown(enumKeyCodes key, double timestamp) {
     MenuLayer::keyDown(key, timestamp);
     if (advanceDeveloperKeySequence(s_developerKeyProgress, s_lastDeveloperKeyAt, key))
